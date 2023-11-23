77e3ab77d635875943b661afad26fdb70eba79fe
woongjee <jeewoong7753@naver.com>
2023-11-23 17:59:47 +0900

    최종본

--- play.py
+++ play.py
@@ -1,46 +1,247 @@
 import sys
-import bluemarble_UI
+from bluemarble_UI import *
 from ground import *
 from player import *
-game_Turn = 0
 
-global board
 pygame.init()
+board = Board.make_block()
+users = Users()
+dice = Dice(random.randint(1, 6), random.randint(1, 6))
+def buy_ground (user, board):
+    build = 0
+    while(build == 0): 
+        events=pygame.event.get()
+        for event in events:
+            if event.type == pygame.KEYUP and event.key == pygame.K_b and (board.blocks[int(user.position)].owner!=user) :
+                board.blocks[int(user.position)].purchase_property(user)
+            elif event.type == pygame.KEYUP and event.key == pygame.K_b and board.blocks[int(user.position)].owner == user and board.blocks[int(user.position)].villas==0:
+                board.blocks[int(user.position)].build_villa(user)
+            elif event.type == pygame.KEYUP and event.key == pygame.K_b and board.blocks[int(user.position)].owner == user and board.blocks[int(user.position)].villas==1:
+                board.blocks[int(user.position)].build_hotel(user)
+            elif event.type == pygame.KEYUP and event.key == pygame.K_1:
+                if len(user.items)>=1:
+                    item_num=user.items[0].number
+                    if(item_num==1 or item_num==2):
+                        while(1):
+                            tgt=users.user_list[random.randint(0, 3)]
+                            if(tgt!=user):
+                                break
+                            else:
+                                pass
+                        user.items[0].Use_item_tgt(user,item_num,tgt)
+                        user.items.pop(0)
+                    elif(item_num<=7):
+                        user.items[0].Use_item(user,item_num)
+                        user.items.pop(0)
+            elif event.type == pygame.KEYUP and event.key == pygame.K_2:
+                if len(user.items)>=2:
+                    item_num=user.items[1].number
+                    if(item_num==1 or item_num==2):
+                        while(1):
+                            tgt=users.user_list[random.randint(0, 3)]
+                            if(tgt!=user):
+                                break
+                            else:
+                                pass
+                        user.items[1].Use_item_tgt(user,item_num,tgt)
+                        user.items.pop(1)
+                    elif(item_num<=7):
+                        user.items[1].Use_item(user,item_num)
+                        user.items.pop(1)
+            elif event.type == pygame.KEYUP and event.key == pygame.K_3:
+                if len(user.items)>=3:
+                    item_num=user.items[2].number
+                    if(item_num==1 or item_num==2):
+                        while(1):
+                            tgt=users.user_list[random.randint(0, 3)]
+                            if(tgt!=user):
+                                break
+                            else:
+                                pass
+                        user.items[2].Use_item_tgt(user,item_num,tgt)
+                        user.items.pop(2)
+                    elif(item_num<=7):
+                        user.items[2].Use_item(user,item_num)
+                        user.items.pop(2)
+            elif event.type == pygame.KEYUP and event.key == pygame.K_4:
+                if len(user.items)>=4:
+                    item_num=user.items[3].number
+                    if(item_num==1 or item_num==2):
+                        while(3):
+                            tgt=users.user_list[random.randint(0, 3)]
+                            if(tgt!=user):
+                                break
+                            else:
+                                pass
+                        user.items[3].Use_item_tgt(user,item_num,tgt)
+                        user.items.pop(3)
+                    elif(item_num<=7):
+                        user.items[3].Use_item(user,item_num)
+                        user.items.pop(3)
+            elif event.type == pygame.KEYUP and event.key == pygame.K_5:
+                if len(user.items)>=5:
+                    item_num=user.items[4].number
+                    if(item_num==1 or item_num==2):
+                        while(1):
+                            tgt=users.user_list[random.randint(0, 3)]
+                            if(tgt!=user):
+                                break
+                            else:
+                                pass
+                        user.items[4].Use_item_tgt(user,item_num,tgt)
+                        user.items.pop(4)
+                    elif(item_num<=7):
+                        user.items[4].Use_item(user,item_num)
+                        user.items.pop(4)
+            elif event.type == pygame.KEYUP and event.key == pygame.K_t:
+                build+=1
+                break 
 
 class PygameWindow:
     def __init__(self):
-        pygame.init()
+        pygame.init() 
         self.width = WIDTH
         self.height = HEIGHT
         self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
         self.clock = pygame.time.Clock()
-        self.current_screen = START_MENU(self.screen)
+        self.current_screen = START_MENU(self.screen, board, users, dice, log_instance)
+        
+    
 
     def run_game(self):
-        board = Board.make_block()
+        current_user_index = 0
+        game_turn=0
         while True:
             events = pygame.event.get()
             for event in events:
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
-                #if User.position == 10:#여기서 말하는 User는 현재 플레이하는 유저
-                #    if  User.roll_dice_event()== True:
-                #        User.move()
-                #if User.position == 20:
-                #    if Board.blocks[20].price !=0:
-                #        User.receive_Tax(,Board.blocks[20])# 현재 플레이하는 user 넣어야함
-                #if User.position == 30:
-            self.current_screen = self.current_screen.handle_events(events)
-            if type(self.current_screen)==bluemarble_UI.GAME_SCREEN:
-                self.current_screen.update()
-                self.current_screen.draw(board)
-            else:
-                self.current_screen.update()
-                self.current_screen.draw()
+                if type(self.current_screen) ==GAME_SCREEN:
+                    user = users.user_list[current_user_index]  # 현재 사용자
+                    if(user.SE):
+                        if event.type == pygame.KEYUP and event.key == pygame.K_SPACE:
+                            if user.position != 10 and user.position !=30:
+                                if dice.roll_dice(user) == True:
+                                    user.move(sum(dice.dice))
+                                    log_instance.add_message("더블")
+                                    self.current_screen.draw(board)
+                                    pygame.display.flip()
+                                    buy_ground(user, board)
+                                    if board.blocks[int(user.position)].owner!=user and board.blocks[int(user.position)].owner !=None:
+                                        board.blocks[int(user.position)].pay_toll(user)
+                                        board.blocks[int(user.position)].owner.money += board.blocks[int(user.position)].toll
+                                    break
+                                else:
+                                    user.move(sum(dice.dice))
+                                    self.current_screen.draw(board)
+                                    pygame.display.flip()
+                                    buy_ground(user, board)
+                                if board.blocks[int(user.position)].owner!=user and board.blocks[int(user.position)].owner !=None:
+                                    board.blocks[int(user.position)].pay_toll(user)
+                                    board.blocks[int(user.position)].owner.money += board.blocks[int(user.position)].toll
+                            if user.position == 2 or user.position==7 or user.position == 12 or user.position == 17 or user.position == 22 or user.position == 35:
+                                user.add_item()
+                            if user.position == 30:
+                                log_instance.add_message("우주")
+                                move=0
+                                while(move==0):
+                                    events = pygame.event.get()
+                                    for event in events:
+                                        if event.type == pygame.MOUSEBUTTONDOWN:
+                                            if event.button == 1:
+                                                x, y = pygame.mouse.get_pos()
+                                                wantposition = board.get_block_at_position(self.current_screen.Ground_Click(x, y)).position
+                                                user.position = wantposition
+                                                buy_ground(user, board)
+                                                if board.blocks[int(user.position)].owner!=user and board.blocks[int(user.position)].owner !=None:
+                                                    board.blocks[int(user.position)].pay_toll(user)
+                                                    board.blocks[int(user.position)].owner.money += board.blocks[int(user.position)].toll
+                                                move+=1
+                                                self.current_screen.draw(board)
+                                                pygame.display.flip()
+                                                buy_ground(user, board)
+                                
+                            
+                            if user.position == 10 and user.jail_turn == 0:
+                                user.jail_turn += 1
+                                log_instance.add_message("무인도에 도착 현재 제일턴 1")
+                            elif user.position == 10 and user.jail_turn == 3:
+                                if dice.roll_dice(user) == True:
+                                    user.move(sum(dice.dice))
+                                    log_instance.add_message("더블")
+                                    break
+                                user.move(sum(dice.dice))
+                                user.jail_turn = 0
+                            elif user.position == 10:
+                                if dice.roll_dice(user) == True:
+                                    user.move(sum(dice.dice))
+                                    log_instance.add_message("더블이 나와 탈출에 성공하셨습니다.")
+                                else:
+                                    user.jail_turn += 1
+                                    log_instance.add_message(f"유저의 제일턴 : {user.jail_turn}")
 
-            pygame.display.flip()
+                            if user.position == 20:
+                                log_instance.add_message("복지")
+                                if board.blocks[20].price != 0:
+                                    user.money += board.blocks[20].price
+                                    board.blocks[20].price = 0
+                            
+                    
+                            if user.SE:
+                                users.user_list[current_user_index].turns += 1
+                                current_user_index = (current_user_index + 1) % (len(self.current_screen.users))
+                                if users.user_list[0].turns==users.user_list[len(users.user_list)-1].turns:
+                                    game_turn+=1
+                                    log_instance.add_message(f"게임턴: {game_turn}") 
+                                if game_turn==20:
+                                    self.current_screen = Ending(self.screen, users)
+                    if user.SE == False:
+                        users.user_list[current_user_index].turns += 1
+                        current_user_index = (current_user_index + 1) % (len(self.current_screen.users))
+                        if users.user_list[0].turns==users.user_list[len(users.user_list)-1].turns:
+                            game_turn+=1
+                            log_instance.add_message(f"게임턴: {game_turn}")
+                    count = 0
+                    for user in users.user_list:
+                        if user.SE == False: 
+                            count += 1
+                        if count == len(users.user_list)-1:
+                            self.current_screen = Ending(self.screen, users)
+                if type(self.current_screen)==Ending:
+                    if event.type == pygame.QUIT or event.type == pygame.MOUSEBUTTONDOWN or event.type == pygame.KEYDOWN:
+                        pygame.quit()
+                        sys.exit()
+
+                          
+
+                        
+            if type(self.current_screen)==START_MENU:          
+                self.current_screen.draw()
+                pygame.display.flip()
+                self.current_screen = self.current_screen.handle_events(events)
+            if type(self.current_screen) == Loading:
+                self.current_screen.draw_loading()
+                pygame.display.flip()
+                self.current_screen = self.current_screen.handle_events(events)
+            if type(self.current_screen)==GAME_SCREEN:
+                
+                self.current_screen.draw(board)                
+                pygame.display.flip()
+                for user in self.current_screen.users.user_list:
+                    if user.ifadd == 1:
+                        pygame.time.delay(3000)
+                        user.ifadd = 0
+                self.current_screen = self.current_screen.handle_events(events)
+                self.clock.tick(60)
+            elif type(self.current_screen) == Ending:
+                self.current_screen.draw()
+                pygame.display.flip()
             self.clock.tick(60)
+
+            
+
+            
 
 if __name__ == "__main__":
     window = PygameWindow()
